4af0d0809dea4cb5793edc3951f21968a0a84d42
Tarmim Hoque Shuvo <tarmim@lotustech.dev>
2020-07-28 10:13:37 +0600

    add organiser, remove video

--- src/screens/EventDetails.js
+++ src/screens/EventDetails.js
@@ -47,123 +47,7 @@ const EventDetails: () => React$Node = ({navigation}) => {
     navigation.navigate("TicketDetails",{item,cover:BaseUrl+eventData.cover.full})
 
   }
-  const [eventData,setEventData]= useState(
-    {
-      id:"1",
-      image: "https://app.imagineradio.io/media/album/art/default.jpg",
-      brand:"",
-      title:"",
-      details:"",
-      topCategory:[
-        {
-          id:"1",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Bata",
-          title:"A great event"
-        },
-        {
-          id:"2",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Samsung",
-          title:"event at ICCB"
-        },
-        {
-          id:"3",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Apple",
-          title:"Basundhara city"
-        },
-        {
-          id:"4",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Xiaomi",
-          title:"A bad event"
-        },
-        {
-          id:"5",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"NMH",
-          title:"event at dit"
-        },
-        {
-          id:"6",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"DHT",
-          title:"Basundhara complex"
-        }
-      ],
-      relatedEvents:[
-        {
-          id:"1",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Bata",
-          title:"A great event",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        },
-        {
-          id:"2",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Samsung",
-          title:"event at ICCB",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        },
-        {
-          id:"3",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Apple",
-          title:"Basundhara city",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        },
-        {
-          id:"4",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"Xiaomi",
-          title:"A bad event",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        },
-        {
-          id:"5",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"NMH",
-          title:"event at dit",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        },
-        {
-          id:"6",
-          image: "https://app.imagineradio.io/media/album/art/default.jpg",
-          brand:"DHT",
-          title:"Basundhara complex",
-          details:" Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, ",
-    
-        }
-      ],
-
-    })
-    const priceData=[
-      {
-        id:'1',
-        type:'Premium',
-        price:510,
-        available:120
-      },
-      {
-        id:'2',
-        type:'Gold',
-        price:710,
-        available:120
-      },
-      {
-        id:'3',
-        type:'Silver',
-        price:810,
-        available:120
-      },
-    ]
+  const [eventData,setEventData]= useState({})
     if (loading)
     return <ActivityIndicator/>
     return (
@@ -260,7 +144,9 @@ const EventDetails: () => React$Node = ({navigation}) => {
         >
         {
           eventData.pricing.map(price=>
-            <View>
+            <View
+            key={price._id}
+            >
               <View
                 style={{
                   height:window.width/10,
@@ -398,7 +284,8 @@ const EventDetails: () => React$Node = ({navigation}) => {
           backgroundColor:'#00163D',
           borderRadius: 25,
           height:window.height/4,
-          flexDirection:'row'
+          flexDirection:'row',
+          elevation:10
         }}
         >
           <TouchableOpacity
@@ -409,9 +296,10 @@ const EventDetails: () => React$Node = ({navigation}) => {
             borderRadius:25,
           }}
           onPress={()=>setModalVisible(true)}
+          disabled={true}
           >
           <Image
-            source={{uri:BaseUrl+eventData.cover.full}}
+            source={{uri:BaseUrl+eventData.image[0].full}}
           style={{
             width: window.width/2,
             height:window.width/2.5,
@@ -420,7 +308,7 @@ const EventDetails: () => React$Node = ({navigation}) => {
 
         }}
           />
-          <Image
+          {/* <Image
 					style={{
             position:'absolute',
             top:window.height / 12.5,
@@ -429,7 +317,7 @@ const EventDetails: () => React$Node = ({navigation}) => {
             height: window.height / 15,
           }}
 					source={iconPlay}
-				/>
+				/> */}
           </TouchableOpacity>
           
           <View
@@ -494,6 +382,96 @@ const EventDetails: () => React$Node = ({navigation}) => {
             darkText
             title="Related Events"
         /> */}
+        <Text
+              style={{
+                color:'#00163D',
+                fontSize:18,
+                marginTop:-5,
+                marginLeft:23,
+                marginBottom:15,
+                fontWeight:'bold'
+              }}
+              >
+          Organiser
+        </Text>
+      <View
+        style={{
+          marginHorizontal:10,
+          marginBottom:window.height/25,
+          backgroundColor:'#00163D',
+          borderRadius: 25,
+          height:window.height/3,
+          elevation:10
+        }}
+        >
+          <View
+          style={{
+            width:window.width/1.2,
+            height:window.height/8,
+            alignSelf:'center',
+            justifyContent:'center',
+            // backgroundColor:'red'
+          }}
+          >
+              <Text
+              numberOfLines={2}
+              style={{
+                fontSize:15,
+                color:'white',
+                fontWeight:'700',
+                alignSelf:'center'
+            }}
+              >
+                {eventData.brand.name}
+              </Text>
+              <Text
+                style={{
+                  fontSize:10,
+                  color:'white',
+                  fontWeight:'700',
+                  alignSelf:'center'
+                }}
+                >
+                {eventData.brand.description}
+                </Text>
+          </View>
+          <TouchableOpacity
+          style={{
+            width: window.width/2,
+            height: window.width/3,
+            margin:window.width/50,
+            borderRadius:25,
+            alignSelf:'center'
+          }}
+          onPress={()=>setModalVisible(true)}
+          disabled={true}
+          >
+          <Image
+            source={{uri:BaseUrl+eventData.brand.cover.full}}
+          style={{
+            width: window.width/2,
+            height:window.width/3,
+            resizeMode: 'cover',
+            borderRadius:25,
+
+        }}
+          />
+          {/* <Image
+					style={{
+            position:'absolute',
+            top:window.height / 12.5,
+            left:window.height / 9.5,
+            width: window.height / 15,
+            height: window.height / 15,
+          }}
+					source={iconPlay}
+				/> */}
+          </TouchableOpacity>
+          
+          
+          
+        </View>
+        
         <CategoryList
         title="Related Categories"
         navigation={navigation}
